55ea5a7804e9c4e74d4816ace4d34466f18f94f2
brunonettom <brunonm@al.insper.edu.br>
2024-11-23 21:51:12 -0300

    resolvido - tecnica com o logger em excesso

--- avaliacao_ai/q1.py
+++ avaliacao_ai/q1.py
@@ -12,6 +12,8 @@ from robcomp_util.laser import Laser
 from robcomp_util.odom import Odom
 from cv_bridge import CvBridge
 from robcomp_util.odom import Odom  # Importando a classe Odom
+# from robcomp_util.amcl import AMCL
+from geometry_msgs.msg import Twist, Point
 
 class Circuito(Node, Odom, Laser):
 
@@ -57,7 +59,7 @@ class Circuito(Node, Odom, Laser):
         time.sleep(1)
         self.timer = self.create_timer(0.05, self.control)
 
-        self.robot_state = 'alvo_novo'
+        self.robot_state = 'segue'
         self.state_machine = {
             'segue': self.segue,
             'girar': self.girar,
@@ -65,27 +67,40 @@ class Circuito(Node, Odom, Laser):
             'alvo_novo': self.alvo_novo,
             'segue_novo': self.segue_novo,
             'girar': self.girar,
-            'center': self.center,
             'goto': self.goto,
-
+            'goto': self.goto,
+            'stop': self.stop
         }
-        voltando_flag = False
-        self.threshold = 5
+
+        # Inicialização de variáveis
         self.twist = Twist()
 
         # Inicialização de variáveis
+        self.contador=0
         self.x_i=self.x
         self.y_i=self.y
+        self.pinto=None
         self.cx = np.inf
 
-        self.volta_completa = False
-        self.v_angular=0.3
-        self.vel=0.7
-        self.kp_linear = 0.15  # Constante proporcional para o controle linear
-        self.kp_angular = 1.2
+        # constantes
+        self.threshold = 5
+        self.vel=0.5
+        self.twist = Twist()
+        self.kp_linear=0.4
+        self.kp_angular=0.5
+        self.kp=0.007
+        self.rodancia=0.25
+        flag_voltando = False
+        self.intervalo_do_contador=20
 
-        self.chegou_flag = False
-        self.cacando_flag = False
+        self.max_vel = 0.3
+
+        # flags
+        self.flag_contador_resetado=False
+        self.flag_chegou = False
+        self.flag_cacando = False
+        self.flag_voltando = False
+        self.flag_volta_completa = False
 
 
         # Publishers
@@ -96,7 +111,8 @@ class Circuito(Node, Odom, Laser):
         h,w,_ = cv_image.shape
         self.w = w/2
         hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-        self.get_logger().info(f'lower: {self.cores[self.cor]["lower"]}; upper: {self.cores[self.cor]["upper"]}')
+        # if self.flag_voltando == False:
+            # self.get_logger().info(f'lower: {self.cores[self.cor]["lower"]}; upper: {self.cores[self.cor]["upper"]}')
         # self.get_logger().info('Cor: {}'.format(self.cor))
         mask = cv2.inRange(hsv, self.cores[self.cor]['lower'], self.cores[self.cor]['upper'])
         mask[:int(h/2),:] = 0
@@ -105,9 +121,13 @@ class Circuito(Node, Odom, Laser):
         
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        cv2.imshow("cv_image", mask)
-        cv2.waitKey(1)
-        self.get_logger().info(f'len(contours): {len(contours)}')
+        if self.flag_voltando == False:
+            cv2.imshow("cv_image", mask)
+            cv2.waitKey(1)
+            # self.get_logger().info(f'len(contours): {len(contours)}')
+        else:
+            cv2.imshow("cv_image", cv_image)
+            cv2.waitKey(1)
         if len(contours) > 0:
             contour = max(contours, key=cv2.contourArea)
             if cv2.contourArea(contour) > 1:
@@ -131,32 +151,35 @@ class Circuito(Node, Odom, Laser):
             self.cx = np.inf
         
     def segue(self):
+
         dist=((self.x-self.x_i)**2+(self.y-self.y_i)**2)**1/2
 
 
-        if self.volta_completa == True:
+        if self.flag_volta_completa == True:
             if (dist<=0.9):
                 self.robot_state='para'
         if abs(self.x_i-self.x)>3:
-            self.volta_completa = True
+            self.flag_volta_completa = True
 
         
         if self.cx==np.inf:
-            self.twist.angular.z = self.v_angular
+            self.twist.angular.z = 0.3
         else:
             erro = self.w - self.cx            
             self.twist.linear.x = self.vel
-            self.twist.angular.z=self.kp_angular*erro
+            self.twist.angular.z=self.kp*erro
 
     def para(self):
+
         self.twist = Twist()
         self.goal_yaw = self.yaw+np.pi/2
-        if self.cacando_flag == True and self.voltando_flag == True:
-            self.robot_state='center'
-        elif self.chegou_flag== False:
+        if self.flag_cacando == True and self.flag_voltando == True:
+            self.robot_state='goto'
+        elif self.flag_chegou== False:
             self.robot_state='girar'
         
     def girar(self):
+
         erro=self.goal_yaw-self.yaw
         erro=np.arctan2(np.sin(erro),np.cos(erro))
         if erro<np.deg2rad(2) and erro>np.deg2rad(-2):
@@ -166,73 +189,90 @@ class Circuito(Node, Odom, Laser):
             self.tempo_objetivo=int(self.tempo_objetivo.sec)+5
             self.robot_state='alvo_novo'
         else:
-            self.twist.angular.z=self.v_angular
+            self.twist.angular.z=self.rodancia
     
     def alvo_novo(self):
+
         self.cor=self.cor_nova
-        self.cacando_flag = True
-        self.get_logger().info(f'Nova cor: {self.cor}')
+        self.flag_cacando = True
+        self.get_logger().info(f'NOVA COOOOOOOOOOOOOOOOOOOOOOOOOR: {self.cor}')
         self.robot_state='segue_novo'        
 
     def segue_novo(self):
-        
+
         if self.cx==np.inf:
-            self.twist.angular.z = self.v_angular
+            self.twist.angular.z = 0.3
         else:
             erro_angular = self.w - self.cx            
-            self.twist.angular.z=self.kp_angular*erro_angular
+            self.twist.angular.z=self.kp*erro_angular
             self.distancia = min(self.front)
             if self.distancia > 10:
                 self.distancia = 3
             self.twist.linear.x = 0.1 * self.distancia
-            if self.distancia < 0.19:
+            if self.distancia < 0.3:
                 self.twist.linear.x = 0.0
                 self.twist.angular.z = 0.0
                 self.goal_yaw = self.yaw + np.pi
+                self.get_logger().info(f'Chegou ao creeper - distancia: {self.distancia:.2f} \n ')
                 self.robot_state = 'para'
-                self.voltando_flag = True
+                self.flag_contador_resetado = False
+                self.flag_voltando = True
 
     '''
     goto
     '''
-    def center(self):
-        # faz o robô girar até que ele esteja 
-        # alinhado com o ponto desejado.
-        delta_x = self.x_i - self.x
-        delta_y = self.y_i - self.y
-        angulo = math.atan2(delta_y, delta_x)
-        erro = math.atan2(math.sin(angulo - self.yaw), math.cos(angulo - self.yaw))
-        print(f'erro:{math.degrees(erro):.3f}')
-        print(f'angulo:{math.degrees(angulo):.3f}')
-        print(f'yaw:{math.degrees(self.yaw):.3f}')
-        print(f'x_i: {self.x_i}')
-        print(f'y_i: {self.y_i}')
-        print(f'delta_x: {delta_x}')
-        print(f'delta_y: {delta_y}')
-        print(f'tg: {math.tan(angulo)}')
-        if abs(math.degrees(erro)) > 5:
-            self.twist.angular.z = self.kp_angular * erro
-        else:
-            self.robot_state = 'goto'
 
-    def acha_distancia(self, x0, x1, y0, y1):
-        return math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2)
+    def get_angular_error(self):
+        deltax=self.x_i-self.x
+        deltay=self.y_i-self.y
+        distancia=((deltax)**2+(deltay)**2)**.5
+        angulo=np.arctan2(deltay,deltax)
+
+        erro=angulo-self.yaw
+        erro=np.arctan2(np.sin(erro),np.cos(erro))
+
+        return distancia,erro
+
+    def stop(self):
+
+        self.twist = Twist()
+        self.twist.angular.z=0.0
+        self.twist.linear.x= 0.0
 
     def goto(self):
-        distancia = self.acha_distancia(self.x, self.x_i, self.y, self.y_i)
-        
-        if distancia > 0.3:
-            self.twist.linear.x = self.kp_linear * distancia
+
+        distancia, erro_yaw = self.get_angular_error()
+        if distancia<0.3:
+            self.twist.angular.z=0.0
+            self.twist.linear.x= 0.0
+            self.robot_state='stop'
+            self.get_logger().info(f'CHEGOUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU!!!!!!!!!!!!! \n\n\n\n\n\ distancia: {distancia:.2f} erro_yaw: {erro_yaw:.2f} \n ')
+            self.get_logger().info(f'v linear: {self.twist.linear.x:.2f}; v angular: {self.twist.angular.z:.2f} \n x_i: {self.x_i:.2f}; y_i: {self.y_i:.2f} \n x: {self.x:.2f}; y: {self.y:.2f} \n\n\n\n\n')
+
+        elif erro_yaw<np.deg2rad(2) and erro_yaw>np.deg2rad(-2):
+            self.twist.angular.z=0.0
+            self.twist.linear.x=distancia*self.kp_linear
+            if self.contador%self.intervalo_do_contador==0:
+                self.get_logger().info(f'erro_yaw pequeno: {erro_yaw:.2f} e distancia grande: {distancia:.2f} \n ')
+                self.get_logger().info(f'v linear: {self.twist.linear.x:.2f}; v angular: {self.twist.angular.z:.2f} \n x_i: {self.x_i:.2f}; y_i: {self.y_i:.2f} \n x: {self.x:.2f}; y: {self.y:.2f} \n\n\n\n\n')
+
         else:
-            self.twist.linear.x = 0.0
-            self.chegou_flag = True
-            self.robot_state = 'para'
+            self.twist.angular.z=erro_yaw*self.kp_angular
+            self.twist.linear.x=0.0
+            if self.contador%self.intervalo_do_contador==0:
+                self.get_logger().info(f'erro_yaw grande: {erro_yaw:.2f} e distancia grande: {distancia:.2f} \n ')
+                self.get_logger().info(f'v linear: {self.twist.linear.x:.2f}; v angular: {self.twist.angular.z:.2f} \n x_i: {self.x_i:.2f}; y_i: {self.y_i:.2f} \n x: {self.x:.2f}; y: {self.y:.2f} \n\n\n\n\n')
+
+            
+            # self.twist.linear.x=distancia*self.kp_linear
 
 
 
     def control(self):
         self.twist = Twist()
-        print(f'Estado Atual: {self.robot_state}')
+        self.contador+=1
+        if self.contador%(self.intervalo_do_contador*5)==0:
+            print(f'ESTADO ATUAAAAAAAAAAAAAAAAAAAAL: {self.robot_state}')
         self.state_machine[self.robot_state]()
         self.cmd_vel_pub.publish(self.twist)
 
